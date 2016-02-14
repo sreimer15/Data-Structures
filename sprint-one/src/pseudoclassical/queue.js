7712ac6d898999342cadc98de07f2c87a4d315b6
Sean Reimer <sreimer15@gmail.com>
2016-02-13 16:01:11 -0800

    Liberating

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
+/* CODE GOES HERE
+ * Don't forget newlines at the end of all files :) */
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this._storage = {};
+  this.front = 0;
+  this.end = 0;
 };
 
+Queue.prototype.add = function(value) {
+  this.storage[this.end] = value;
+  this.end++;
+};
 
+Queue.prototype.remove = function() {
+  if (this.size() > 0) {
+    var temp = this.storage[this.front];
+    this.front++;
+    return frontValue;
+  }
+};
+
+Queue.prototype.size = function(){
+  this.end - this.front;
+}
